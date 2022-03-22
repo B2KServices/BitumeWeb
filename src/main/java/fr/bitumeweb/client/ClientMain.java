a7fbaeacf0ace36e68a4897dcba62c9940f863ba
timoreo <timo.oreo34@gmail.com>
2022-03-22 21:20:27 +0100

    Ajout d'un peu d'info de debug

--- src/main/java/fr/bitumeweb/client/ClientMain.java
+++ src/main/java/fr/bitumeweb/client/ClientMain.java
@@ -8,14 +8,15 @@ public class ClientMain {
     private static JSWebSocket socket;
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
         String domain = ((CustomWindow) Window.window()).location().hostname();
-        System.out.println(domain);
         socket = JSWebSocket.create("wss://" + domain + ":8443/chat");
         socket.addEventListener("message", aEvent -> {
             String data = Window.window().document().getElementById("log").innerHTML();
             Window.window().document().getElementById("log").innerHTML(data + ((DataEvent) aEvent).data() + "\n");
             System.out.println("Message From Server " + ((DataEvent) aEvent).data());
+        });
+        socket.addEventListener("open", aEvent -> {
+            System.out.println("Démarré !");
         });
     }
 
