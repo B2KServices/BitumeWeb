af5541cb204e249f21fb2eaa2af558b6ddfb0ecf
timoreo <timo.oreo34@gmail.com>
2022-03-22 16:21:59 +0100

    Test SSL

--- src/main/java/fr/bitumeweb/Main.java
+++ src/main/java/fr/bitumeweb/Main.java
@@ -4,10 +4,14 @@ import com.google.gson.*;
 import com.sun.net.httpserver.*;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
 import org.java_websocket.server.WebSocketServer;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
@@ -16,7 +20,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.Map;
 
 import static java.nio.file.FileVisitOption.FOLLOW_LINKS;
@@ -64,6 +78,11 @@ public class Main {
 
             }
         };
+        SSLContext context = getContext();
+        if (context != null) {
+            server.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(context));
+        }
+        server.setConnectionLostTimeout(30);
         server.start();
         System.out.println("Starting server...");
         Gson gson = new Gson();
@@ -167,6 +186,77 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static SSLContext getContext() {
+        SSLContext context;
+        String password = "ThisDoesNotMatter";
+        String pathname = "certs";
+        try {
+            context = SSLContext.getInstance("TLS");
+
+            byte[] certBytes = parseDERFromPEM(getBytes(new File(pathname + File.separator + "cert.pem")),
+                    "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----");
+            byte[] keyBytes = parseDERFromPEM(
+                    getBytes(new File(pathname + File.separator + "privkey.pem")),
+                    "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----");
+
+            X509Certificate cert = generateCertificateFromDER(certBytes);
+            RSAPrivateKey key = generatePrivateKeyFromDER(keyBytes);
+
+            KeyStore keystore = KeyStore.getInstance("JKS");
+            keystore.load(null);
+            keystore.setCertificateEntry("cert-alias", cert);
+            keystore.setKeyEntry("key-alias", key, password.toCharArray(), new Certificate[]{cert});
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(keystore, password.toCharArray());
+
+            KeyManager[] km = kmf.getKeyManagers();
+
+            context.init(km, null, null);
+        } catch (Exception e) {
+            context = null;
+        }
+        return context;
+    }
+
+    private static byte[] parseDERFromPEM(byte[] pem, String beginDelimiter, String endDelimiter) {
+        String data = new String(pem);
+        String[] tokens = data.split(beginDelimiter);
+        tokens = tokens[1].split(endDelimiter);
+
+        return Base64.getDecoder().decode(tokens[0]);
+    }
+
+    private static RSAPrivateKey generatePrivateKeyFromDER(byte[] keyBytes)
+            throws InvalidKeySpecException, NoSuchAlgorithmException {
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+
+        KeyFactory factory = KeyFactory.getInstance("RSA");
+
+        return (RSAPrivateKey) factory.generatePrivate(spec);
+    }
+
+    private static X509Certificate generateCertificateFromDER(byte[] certBytes)
+            throws CertificateException {
+        CertificateFactory factory = CertificateFactory.getInstance("X.509");
+
+        return (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(certBytes));
+    }
+
+    private static byte[] getBytes(File file) {
+        byte[] bytesArray = new byte[(int) file.length()];
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            fis.read(bytesArray); //read file into bytes[]
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytesArray;
     }
 
     private static void replaceStuff() {
